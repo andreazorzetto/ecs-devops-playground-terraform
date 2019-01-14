91bd3561d633042e2ef1a8b3f4313ab6f02d8c48
Andrea Zorzetto <andrea.zorzetto@ecs-digital.co.uk>
2019-01-14 17:15:09 +0000

    requirements pre-installation

--- infrastructure/scripts/user_data_user.sh
+++ infrastructure/scripts/user_data_user.sh
@@ -2,7 +2,7 @@
 
 # Basic software
 yum install -y epel-release
-yum install -y curl wget make gcc-c++ git htop vim
+yum install -y curl wget make gcc-c++ git htop vim jq
 yum groupinstall -y 'Development Tools'
 
 # SSH User
@@ -36,5 +36,56 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
+# SELINUX
+
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+
+# Docker
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y --setopt=obsoletes=0 docker-ce-17.03.3.ce-1.el7 docker-ce-selinux.noarch-17.03.3.ce-1.el7
+
+systemctl enable docker
+systemctl start docker
+
+usermod -G docker centos
+
+# Kubernetes
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum -y install kubelet-1.11.3-0 kubeadm-1.11.3-0 kubectl-1.11.3-0
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl --system
+
+su - centos -c "git clone https://github.com/andreazorzetto/ecs-devops-playground-resources.git"
+
+# Wetty
 systemctl enable wetty.service
 systemctl start wetty.service
+
+
+
+
+
+
+
+
+
+
+
+
+
+
